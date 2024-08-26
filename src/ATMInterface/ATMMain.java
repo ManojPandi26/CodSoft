0213bb496e9920a5443419ed931eb0d6fcf0f9b7
ManojPandi26 <155637178+ManojPandi26@users.noreply.github.com>
2024-08-26 13:49:36 +0530

    CodSoft_Task_3

--- src/ATMInterface/ATMMain.java
+++ src/ATMInterface/ATMMain.java
@@ -1,7 +1,6 @@
 package ATMInterface;
 
 public class ATMMain {
-
 	 public static void main(String[] args) {
 	        BankAccount account = new BankAccount(1000);
 	        ATM atm = new ATM(account);
